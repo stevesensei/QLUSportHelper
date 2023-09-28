197569f116f05c8f75c875eb686512de40553699
stevesensei <wxtalt@gmail.com>
2023-09-28 22:42:03 +0800

    Initial

---
+++ QluSportHelper/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Fiddler;
+using log4net;
+using log4net.Config;
+using log4net.Core;
+using QluSportHelper.Token;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
+namespace QluSportHelper;
+public class MainProgram
+{
+    private static readonly log4net.ILog logger = log4net.LogManager.GetLogger("主程序");
+    public static async Task Main(String[] args)
+    {
+        //XmlConfigurator.Configure();
+        logger.Info("未检测到token数据,接下来将开始抓取");
+        logger.Info("如果您知道您的token,请关闭程序并在程序目录下的token文件中输入");
+        logger.Info("如果您不知道,请输入y");
+        var wait = Console.ReadLine();
+        if (wait.ToLower() == "y")
+        {
+            logger.Info("即将开始抓取");
+            logger.Warn("请在接下来弹出的提示框中选择 是 ,否则将无法抓取");
+            TokenCapture token = new TokenCapture(8998);
+        }
+        await Task.Delay(-1);
+    }
+}
---
+++ QluSportHelper/TokenHelper/TokenCapture.cs
@@ -0,0 +1,74 @@
+﻿using Fiddler;
+using log4net;
+
+namespace QluSportHelper.Token;
+
+public class TokenCapture
+{
+    private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger("Token抓取器");
+    public TokenCapture(int port)
+    {
+        FiddlerCoreStartupSettingsBuilder builder = new FiddlerCoreStartupSettingsBuilder();
+        builder.ListenOnPort((ushort)port);
+        builder.AllowRemoteClients();
+        builder.RegisterAsSystemProxy();
+        builder.DecryptSSL();
+        var config = builder.Build();
+        FiddlerApplication.Startup(config);
+        InstallCertificate();
+        _logger.Warn("证书安装成功!注意此时您无法正常浏览网页,抓取结束后方可恢复");
+        _logger.Info("接下来请登录微信,打开觅幂小程序,随意点击几处,程序将自动抓取token");
+        FiddlerApplication.BeforeRequest += CaptureToken;
+
+    }
+
+    public void Uninstall()
+    {
+        UninstallCertificate();
+    }
+
+    public static bool InstallCertificate()
+    {
+        if (!CertMaker.rootCertExists())
+        {
+            if (!CertMaker.createRootCert())
+                return false;
+
+            if (!CertMaker.trustRootCert())
+                return false;
+        }
+
+        return true;
+    }
+    
+    public static bool UninstallCertificate()
+    {
+        if (CertMaker.rootCertExists())
+        {
+            if (!CertMaker.removeFiddlerGeneratedCerts(true))
+                return false;
+        }
+        return true;
+    }
+
+    public void CaptureToken(Session session)
+    {
+        if (session.hostname == "admin.report.mestallion.com")
+        {
+            if (!String.IsNullOrEmpty(session.RequestHeaders["token"]))
+            {
+                FiddlerApplication.BeforeRequest -= CaptureToken;
+                _logger.Info("已经抓取到token!正在写入文件");
+                _logger.Warn("请在下一步的移除窗口中选择确定,否则将无法正确浏览网页!");
+                if (!File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory , "token")))
+                {
+                    File.Create(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "token"));
+                }
+                File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "token"),session.RequestHeaders["token"]);
+                Uninstall();
+                Fiddler.FiddlerApplication.oProxy.Detach();
+                FiddlerApplication.Shutdown();
+            }
+        }
+    }
+}
